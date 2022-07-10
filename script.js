99afe2ddb94c730eb0f3061a133544b1407c374f
Josh <jlee12297@gmail.com>
2022-07-09 20:14:51 -0700

    javascript and html changes added jumbotron to test playerheadshot grabbing function in javascript. page now has the functionality to search player name without case sensitivity, and display the matching headshot to the page.

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ fetch(nbaUrl)
   })
   */
 
+//user input searches for player, split input into first name and last name values for searching
 var searchButton = $('#searchButton');
 searchButton.on('click', function() {
 
@@ -24,7 +25,9 @@ searchButton.on('click', function() {
     console.log("Player Last Name: " + searchName[1]);
     searchFirstName = searchName[0];
     searchLastName = searchName[1];
+    
 
+// api to fetch user input to match player name, fetch playerID for getting portrait
 playerUrl = 'https://data.nba.net/data/10s/prod/v1/2020/players.json';
 
 fetch(playerUrl)
@@ -35,11 +38,19 @@ fetch(playerUrl)
     console.log(data.league.standard)
     console.log(data.league.standard[0].firstName)
 
+    //match user search to player name in API, get desired info
     for (i = 0; i < data.league.standard.length; i++) {
         var firstName = data.league.standard[i].firstName
         var lastName = data.league.standard[i].lastName
-        if (searchFirstName === firstName && searchLastName === lastName) {
+        if (searchFirstName.toUpperCase() === firstName.toUpperCase() && searchLastName.toUpperCase() === lastName.toUpperCase()) {
             console.log(firstName + " " + lastName)
+            playerID = data.league.standard[i].personId
+            console.log(playerID)
+            //display player headshot
+            playerHeadshot = document.getElementById("playerHeadshot")
+            src = 'https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/' + playerID + '.png'
+            playerHeadshot.src = src
+
         }
     }
 
